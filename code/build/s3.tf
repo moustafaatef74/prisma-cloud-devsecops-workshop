fa1bf28f8bc593cdbade492941939213fef5bae5
Moustafa Atef <moustafa.atef@nawy.com>
2025-06-11 19:36:10 +0300

    Create s3.tf

---
+++ code/build/s3.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "dev_s3" {
+  bucket_prefix = "dev-"
+
+  tags = {
+    Environment          = "Dev"
+    git_commit           = "d4c35e0270bfd542051278ca30b4b3872c1ae0b2"
+    git_file             = "code/simple_s3.tf"
+    git_last_modified_at = "2024-01-26 23:01:56"
+    git_last_modified_by = "tprendervill@paloaltonetworks.com"
+    git_modifiers        = "tprendervill"
+    git_org              = "moustafaatef74"
+    git_repo             = "prisma-cloud-devsecops-workshop"
+    yor_name             = "dev_s3"
+    yor_trace            = "1f22d4da-2123-4143-9695-3ffb972299fe"
+  }
+}
